7e44d1ad5de6837246131c2087c8a33ab78fcd05
blackkmoon <blackmoonpy@gmail.com>
2018-02-27 14:31:41 -0800

    adding one file

---
+++ test_args.py
@@ -0,0 +1,13 @@
+list_of_results = []
+
+
+def add(a, b):
+    return a + b
+
+
+f = {'a': add}
+
+for i, j in zip(range(3), range(3)):
+    list_of_results.append(f['a'](i, j))
+
+print(list_of_results)
